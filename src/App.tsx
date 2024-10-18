9609f261ae45303662d178f04a1cb6ed3c344f47
gremble0 <hermanstornes@gmail.com>
2024-10-18 11:25:49 +0200

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+
+type Producer = {
+  id: number;
+  name: string;
+  things: Thing[];
+};
+
+type Thing = {
+  id: number;
+  name: string;
+  price: number;
+  producer: Producer;
+};
+
 function App() {
+  const [things, setThings] = useState<Thing[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/things")
+      .then((res) => res.json())
+      .then((body) => setThings(body));
+  }, []);
+
   return (
     <>
-      <p>f</p>
+      <div>
+        {things.map((thing) => (
+          <div key={thing.id}>
+            <p>Name: {thing.name}</p>
+            <p>Price: {thing.price}</p>
+            <p>Producer: {thing.producer.name}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
